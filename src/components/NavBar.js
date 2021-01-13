23e301aa6b7b12909a2c44cbc653097bb7f25dab
sally al sardouk <sally.sardouk98@gmail.com>
2021-01-13 10:36:39 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,9 @@ const NavBar = () => {
   return (
     <div className="navbar">
     <li><NavLink to="/" exact>Home</NavLink></li>
-    <li><NavLink to="/movies"
+    <li><NavLink to="/movies" exact>Movies</NavLink></li>
+    <li><NavLink to="/actors" exact>Actors</NavLink></li>
+    <li><NavLink to="/Directors" exact>Directors</NavLink></li>
     </div>
   );
 };
