dac646e8323ba973a642f2dd03a7c79f029bda21
sally al sardouk <sally.sardouk98@gmail.com>
2021-01-13 10:08:32 +0000

    Automatically backed up by Learn

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,6 +15,10 @@ const App = (props) => {
     <Router>
       {/*{code here}*/}
       </NavBar>
+      <Route exact path="/" component={Home} />
+        <Route exact path="/actors" component={Actors} />
+        <Route exact path="/directors" component={Directors} />
+        <Route exact path="/movies" component={Movies} />
     </Router>
   );
 };
